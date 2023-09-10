58bb9288d13ed97f05944aa139988701666de284
BO ZHANG <bozhang@nao.cas.cn>
2023-09-10 12:53:46 +0800

    tweaks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,6 +58,9 @@ html_theme = 'furo'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_title = "my scratch board"
+html_short_title = "hypergravity"
+html_codeblock_linenos_style = "table"
 
 # 设置输出格式为 "html"，将其改为 "latex"
 # 例如：html_theme = 'alabaster'
