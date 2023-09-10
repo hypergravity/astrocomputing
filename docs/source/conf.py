49b01c68f54556d7bceb552ade8a73bf727eef98
BO ZHANG <bozhang@nao.cas.cn>
2023-09-10 20:36:12 +0800

    add nbsphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,7 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'nbsphinx',
     "recommonmark",
     "sphinx_copybutton",
     "sphinx.ext.autodoc",
